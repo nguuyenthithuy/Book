5b3a2d2a443a0de66ba3dc40e0c9ed4b5193ebdd
nguuyenthithuy <syn8749@gmail.com>
2024-06-05 15:53:12 +0700

    react-router-dom

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./pages/login";
+
+const Layout = () => {
+  return (
+    <>
+      <div>Main page</div>
+    </>
+  );
+};
+export default function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      errorElement: <div>404 not found</div>,
+    },
+    {
+      path: "/login",
+      element: <LoginPage />,
+    },
+  ]);
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
