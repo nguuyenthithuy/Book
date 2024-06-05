233682d86b9e5c3978bd0f09477eb85fa01a3671
nguuyenthithuy <syn8749@gmail.com>
2024-06-05 16:35:08 +0700

    chia layout và project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import LoginPage from "./pages/login";
+import ContactPage from "./pages/contact";
+import BookPage from "./pages/book";
+import Header from "./components/Header";
 
+import Footer from "./components/Footer";
+import Home from "./components/Home";
 const Layout = () => {
   return (
     <>
-      <div>Main page</div>
+      <Header />
+      <Outlet />
+      <Footer />
     </>
   );
 };
@@ -15,6 +22,18 @@ export default function App() {
       path: "/",
       element: <Layout />,
       errorElement: <div>404 not found</div>,
+
+      children: [
+        { index: true, element: <Home /> },
+        {
+          path: "contact",
+          element: <ContactPage />,
+        },
+        {
+          path: "book",
+          element: <BookPage />,
+        },
+      ],
     },
     {
       path: "/login",
