b64c649b2ee91d20a5d38053b2eabd3be92c8c14
nguuyenthithuy <syn8749@gmail.com>
2024-06-06 15:06:25 +0700

    Design Login And Chức Năng Login

--- src/services/api.js
+++ src/services/api.js
@@ -8,3 +8,9 @@ export const callRegister = (fullName, email, password, phone) => {
     phone,
   });
 };
+export const callLogin = (username, password) => {
+  return axios.post("/api/v1/auth/login", {
+    username,
+    password,
+  });
+};
