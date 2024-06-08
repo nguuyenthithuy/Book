dbfd126758c737ec1c674651db0ebbd4d725da54
nguuyenthithuy <syn8749@gmail.com>
2024-06-09 01:51:13 +0700

    Lấy lại access token khi hết hạn

--- src/utils/axios-customize.js
+++ src/utils/axios-customize.js
@@ -22,6 +22,14 @@ instance.interceptors.request.use(
   }
 );
 
+const handleRefreshToken = async () => {
+  const res = await instance.get("/api/v1/auth/refresh");
+  if (res && res.data) return res.data.access_token;
+  else null;
+};
+
+const NO_RETRY_HEADER = "x-no-retry";
+
 // Add a response interceptor
 instance.interceptors.response.use(
   function (response) {
@@ -29,9 +37,28 @@ instance.interceptors.response.use(
     // Do something with response data
     return response && response.data ? response.data : response;
   },
-  function (error) {
+  async function (error) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
+
+    if (
+      error.config &&
+      error.response &&
+      +error.response.status === 401 &&
+      !error.config.headers[NO_RETRY_HEADER]
+    ) {
+      // return updateToken().then((token) => {
+      //   error.config.headers.xxxx <= set the token
+      //   return axios.request(config);
+      // });
+      const access_token = await handleRefreshToken();
+      error.config.headers[NO_RETRY_HEADER] = "true";
+      error.config.headers["Authorization"] = `Bearer ${access_token}`;
+      localStorage.setItem("access_token", access_token);
+      handleRefreshToken();
+      return instance.request(error.config);
+    }
+
     return error?.response?.data ?? Promise.reject(error);
   }
 );
