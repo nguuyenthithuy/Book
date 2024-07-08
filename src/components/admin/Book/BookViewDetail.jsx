d37c5e54ef58704ca9553c6ce22f47a5c420fb38
nguuyenthithuy <syn8749@gmail.com>
2024-07-08 20:39:22 +0700

    Design chức năng xem chi tiết Book

--- src/components/admin/Book/BookViewDetail.jsx
+++ src/components/admin/Book/BookViewDetail.jsx
@@ -1,6 +1,7 @@
-import { Badge, Descriptions, Divider, Drawer } from "antd";
+import { Badge, Descriptions, Divider, Drawer, Modal } from "antd";
 import moment from "moment";
-
+import { useState } from "react";
+import { Image, Upload } from "antd";
 const UserViewDetail = (props) => {
   const {
     setOpenViewDetail,
@@ -8,12 +9,67 @@ const UserViewDetail = (props) => {
     dataViewDetail,
     setDataViewDetail,
   } = props;
+  const [previewImage, setPreviewImage] = useState("");
+  const [previewOpen, setPreviewOpen] = useState(false);
+  const [previewTitle, setPreviewTitle] = useState("");
+
   const onClose = () => {
     setOpenViewDetail(false);
     setDataViewDetail(null);
   };
 
   console.log(dataViewDetail);
+
+  const getBase64 = (file) =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = (error) => reject(error);
+    });
+
+  const [fileList, setFileList] = useState([
+    {
+      uid: "-1",
+      name: "image.png",
+      status: "done",
+      url: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+    },
+    {
+      uid: "-2",
+      name: "image.png",
+      status: "done",
+      url: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+    },
+    {
+      uid: "-3",
+      name: "image.png",
+      status: "done",
+      url: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+    },
+    {
+      uid: "-4",
+      name: "image.png",
+      status: "done",
+      url: "https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png",
+    },
+  ]);
+
+  const handlePreview = async (file) => {
+    console.log("check file", file);
+    if (!file.url && !file.preview) {
+      file.preview = await getBase64(file.originFileObj);
+    }
+    setPreviewImage(file.url || file.preview);
+    setPreviewOpen(true);
+    setPreviewTitle(
+      file.name || file.url.substring(file.url.lastIndexOf("/") + 1)
+    );
+  };
+
+  const handleChange = ({ fileList: newFileList }) => setFileList(newFileList);
+
+  const handleCancel = () => setPreviewOpen(false);
 
   return (
     <>
@@ -48,6 +104,22 @@ const UserViewDetail = (props) => {
         </Descriptions>
 
         <Divider orientation="left">Ảnh book</Divider>
+        <Upload
+          action="https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload"
+          listType="picture-card"
+          fileList={fileList}
+          onPreview={handlePreview}
+          onChange={handleChange}
+          showUploadList={{ showRemoveIcon: false }}
+        ></Upload>
+        <Modal
+          open={previewOpen}
+          title={previewTitle}
+          footer={null}
+          onCancel={handleCancel}
+        >
+          <img alt="example" style={{ width: "100%" }} src={previewImage} />
+        </Modal>
       </Drawer>
     </>
   );
